e146ef19ee8e4905d793f38ce0284800a85f2587
Raphael F. Oliveira <ledrix@hotmail.com>
2023-10-12 15:26:43 -0300

    new code to be used in several machines

--- src/config/server.ts
+++ src/config/server.ts
@@ -3,7 +3,7 @@ import express from "express";
 require("dotenv").config();
 const dated = require("date-and-time");
 
-const PORT: string | number = process.env.PORT || 5000;
+const PORT: string | number = process.env.PORT || 5001;
 
 const app = express();
 
@@ -12,10 +12,29 @@ app.use(cors());
 app.use(express.json());
 
 var valorDoPix = 0;
-var valorAux = 0;
-var ticket = 1;
 
-app.get("/consulta", async (req, res) => {
+var valordoPixMaquina2 = 0; //txid flaksdfjaskldfj
+
+function converterPixRecebido(valorPix: number) {
+  var valorAux = 0;
+  var ticket = 1;
+  if (valorPix > 0 && valorPix >= ticket) {
+    valorAux = valorPix;
+    valorPix = 0;
+    //creditos
+    var creditos = valorAux / ticket;
+    creditos = Math.floor(creditos);
+    var pulsos = creditos * ticket;
+    var pulsosFormatados = ("0000" + pulsos).slice(-4);
+    return pulsosFormatados;
+  } else {
+    return "0000";
+  }
+}
+
+app.get("/consulta-rafael-mac01-19c97b15", async (req, res) => {
+  var valorAux = 0;
+  var ticket = 1;
   if (valorDoPix > 0 && valorDoPix >= ticket) {
     valorAux = valorDoPix;
     valorDoPix = 0;
@@ -25,6 +44,19 @@ app.get("/consulta", async (req, res) => {
     var pulsos = creditos * ticket;
     var pulsosFormatados = ("0000" + pulsos).slice(-4);
 
+    return res.status(200).json({ retorno: pulsosFormatados });
+  } else {
+    return res.status(200).json({ retorno: "0000" });
+  }
+});
+
+//flaksdfjaskldfj << ALTERAR PARA O TXID DA MAQUINA
+app.get("/consulta-rafael-mac02-lojaFulanoDeTal", async (req, res) => {
+  var pulsosFormatados = converterPixRecebido(valordoPixMaquina2); //<<<<<<ALTERAR PARA O NUMERO DA MAQUINA
+
+  valordoPixMaquina2 = 0; //<<<<<<<<<ALTERAR PARA O NUMERO DA MAQUINA
+
+  if (pulsosFormatados != "0000") {
     return res.status(200).json({ retorno: pulsosFormatados });
   } else {
     return res.status(200).json({ retorno: "0000" });
@@ -51,10 +83,19 @@ app.post("/rota-recebimento", async (req, res) => {
     console.log("Novo chamada a essa rota detectada:");
     console.log(req.body);
 
-    console.log("valor:");
+    console.log("valor do pix recebido:");
+    console.log(req.body.pix[0].valor);
+
     if (req.body.pix) {
-      valorDoPix = req.body.pix[0].valor;
-      console.log(req.body.pix[0].valor);
+      if (req.body.pix[0].txid == "flaksdfjaskldfj") {
+        valordoPixMaquina2 = req.body.pix[0].valor;
+        console.log("Creditando valor do pix na máquina 2");
+      }
+
+      if (req.body.pix[0].txid == "flaksdfjaskldfj") {
+        //valordoPixMaquina3 = req.body.pix[0].valor;
+        console.log("Creditando valor do pix na máquina 3");
+      }
     }
   } catch (error) {
     console.error(error);
@@ -68,14 +109,23 @@ app.post("/rota-recebimento-teste", async (req, res) => {
     console.log("Novo pix detectado:");
     console.log(req.body);
 
-    console.log("valor:");
-    valorDoPix = req.body.valor;
+    // console.log("valor:");
+    // console.log(req.body.valor);
+    // console.log("txid:");
+    // console.log(req.body.txid);
+
+    var txid = req.body.txid;
+    if (txid == "flaksdfjaskldfj") {
+      valordoPixMaquina2 = req.body.valor;
+      console.log("setado valor pix para maquina 2:" + req.body.valor);
+    }
+
     console.log(req.body.valor);
   } catch (error) {
     console.error(error);
     return res.status(402).json({ error: "error: " + error });
   }
-  return res.status(200).json({ mensagem: "pix realizado com sucesso" });
+  return res.status(200).json({ mensagem: "ok" });
 });
 
 //código escrito por Lucas Carvalho em meados de Junho de 2023...
