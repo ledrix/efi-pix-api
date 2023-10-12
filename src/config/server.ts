b1a4e8de57c69d791c4a31be7fc3dd2e1a2d2619
Raphael F. Oliveira <ledrix@hotmail.com>
2023-10-12 16:26:26 -0300

    add new code for several machines receipts

--- src/config/server.ts
+++ src/config/server.ts
@@ -11,9 +11,7 @@ app.use(cors());
 
 app.use(express.json());
 
-var valorDoPix = 0;
-
-var valordoPixMaquina2 = 0; //txid flaksdfjaskldfj
+var valorPixMaquina1 = 0; //kanymachinesrafostecno
 
 function converterPixRecebido(valorPix: number) {
   var valorAux = 0;
@@ -32,29 +30,11 @@ function converterPixRecebido(valorPix: number) {
   }
 }
 
-app.get("/consulta-rafael-mac01-19c97b15", async (req, res) => {
-  var valorAux = 0;
-  var ticket = 1;
-  if (valorDoPix > 0 && valorDoPix >= ticket) {
-    valorAux = valorDoPix;
-    valorDoPix = 0;
-    //creditos
-    var creditos = valorAux / ticket;
-    creditos = Math.floor(creditos);
-    var pulsos = creditos * ticket;
-    var pulsosFormatados = ("0000" + pulsos).slice(-4);
-
-    return res.status(200).json({ retorno: pulsosFormatados });
-  } else {
-    return res.status(200).json({ retorno: "0000" });
-  }
-});
-
 //flaksdfjaskldfj << ALTERAR PARA O TXID DA MAQUINA
-app.get("/consulta-rafael-mac02-lojaFulanoDeTal", async (req, res) => {
-  var pulsosFormatados = converterPixRecebido(valordoPixMaquina2); //<<<<<<ALTERAR PARA O NUMERO DA MAQUINA
+app.get("/query-kany-machine01", async (req, res) => {
+  var pulsosFormatados = converterPixRecebido(valorPixMaquina1); //<<<<<<ALTERAR PARA O NUMERO DA MAQUINA
 
-  valordoPixMaquina2 = 0; //<<<<<<<<<ALTERAR PARA O NUMERO DA MAQUINA
+  valorPixMaquina1 = 0; //<<<<<<<<<ALTERAR PARA O NUMERO DA MAQUINA
 
   if (pulsosFormatados != "0000") {
     return res.status(200).json({ retorno: pulsosFormatados });
@@ -87,14 +67,14 @@ app.post("/rota-recebimento", async (req, res) => {
     console.log(req.body.pix[0].valor);
 
     if (req.body.pix) {
-      if (req.body.pix[0].txid == "flaksdfjaskldfj") {
-        valordoPixMaquina2 = req.body.pix[0].valor;
-        console.log("Creditando valor do pix na máquina 2");
+      if (req.body.pix[0].txid == "kanymachinesrafostecno") {
+        valorPixMaquina1 = req.body.pix[0].valor;
+        console.log("Creditando valor do pix na máquina 1");
       }
 
       if (req.body.pix[0].txid == "flaksdfjaskldfj") {
-        //valordoPixMaquina3 = req.body.pix[0].valor;
-        console.log("Creditando valor do pix na máquina 3");
+        //valordoPixMaquina2 = req.body.pix[0].valor;
+        console.log("Creditando valor do pix na máquina 2");
       }
     }
   } catch (error) {
@@ -116,8 +96,8 @@ app.post("/rota-recebimento-teste", async (req, res) => {
 
     var txid = req.body.txid;
     if (txid == "flaksdfjaskldfj") {
-      valordoPixMaquina2 = req.body.valor;
-      console.log("setado valor pix para maquina 2:" + req.body.valor);
+      valorPixMaquina1 = req.body.valor;
+      console.log("setado valor pix para maquina 1:" + req.body.valor);
     }
 
     console.log(req.body.valor);
